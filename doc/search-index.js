7b3c4e8d6826d64d05b8c5fe5cddf30abdff2bd4
Jovansonlee Cesar <ivanceras@gmail.com>
2017-02-15 19:51:18 +0800

    macro examples

---
+++ doc/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["hello"] = {"doc":"This is the module comment? This is hello","items":[],"paths":[]};
+initSearch(searchIndex);
---
+++ hello.rs
@@ -0,0 +1,29 @@
+//! This is the module comment?
+//! This is hello
+//!
+//! ## MD - the markdown
+//! - here comes the markdown content
+//!
+//! ```bob
+//!                  _____________
+//!  +---------+     \            \        +------------+
+//!  | md+bob  |----->) spongedown )------>| html + svg |
+//!  +---------+     /____________/        +------------+
+//! ```
+//! 
+//! Spongedown converts markdown to html with support for 
+//! [svgbob diagrams](https://github.com/ivanceras/svgbobrus)
+//! 
+//! 
+//! 
+//! |  中文处理 | Data  |   CJK      |
+//! |-----------|-------|------------|
+//! |**Table**  | `are` | supported  |
+//! | as        | well  |            |
+//! 
+//! The next `code block` fenced with `bob` will be rendered into an svg
+//!
+//!
+fn main(){
+    println!("hello");
+}
